de8258ee87b304686f47e585f24755b8a0beb230
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2018-07-18 15:58:12 +0100

    refactored code

--- src/main/scala/com/kurtlogan/life/GameOfLife.scala
+++ src/main/scala/com/kurtlogan/life/GameOfLife.scala
@@ -7,19 +7,17 @@ object GameOfLife {
   import CompassFunctor.ops._
 
   def nextGeneration[F[_]: CompassFunctor](cells: F[Cell]): F[Cell] =
-    cells.cmap(Dead) { c =>
-      c.neighbours match {
-        case TwoNeighbours()   => c.point
-        case ThreeNeighbours() => Alive
-        case _                 => Dead
-      }
+    cells.cmap(Dead) {
+      case c @ TwoNeighbours() => c.point
+      case ThreeNeighbours()   => Alive
+      case _                   => Dead
     }
 
   private object TwoNeighbours {
-    def unapply(cells: Seq[Cell]): Boolean = cells.count(_ == Alive) == 2
+    def unapply(c: Compass[Cell]): Boolean = c.neighbours.count(_ == Alive) == 2
   }
 
   private object ThreeNeighbours {
-    def unapply(cells: Seq[Cell]): Boolean = cells.count(_ == Alive) == 3
+    def unapply(c: Compass[Cell]): Boolean = c.neighbours.count(_ == Alive) == 3
   }
 }
